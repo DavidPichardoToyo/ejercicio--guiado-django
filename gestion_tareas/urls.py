87a39492c55b9b42a4d6497648b72e6f255e1883
DavidPichardoToyo <dpichardot31@gmail.com>
2025-08-19 01:22:10 +0000

    ejercicio

---
+++ gestion_tareas/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import lista_tareas, nueva_tarea, editar_tarea
+
+
+urlpatterns = [
+    path("lista_tareas/", lista_tareas, name="lista_tareas"),
+    path("nueva_tarea/", nueva_tarea, name="nueva_tarea"),
+    path("editar/<int:pk>/", editar_tarea, name="editar_tarea"),
+
+]
